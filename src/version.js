a9fc4f14ecd8345ffb2334850aa98bee2d250613
Andrew Blackburn <skyway.float.0r@icloud.com>
2026-07-31 17:08:59 +0100

    chore: release Orbit Cards 1.0.0

--- src/version.js
+++ src/version.js
@@ -1,8 +1,8 @@
-export const ORBIT_CARDS_VERSION = "0.15.0";
+export const ORBIT_CARDS_VERSION = "1.0.0";
 
 export const CARD_VERSIONS = {
   area: "0.8.3",
-  status: "0.13.3",
+  status: "0.13.4",
   action: "0.6.3",
-  deck: "0.3.0",
+  deck: "0.4.0",
 };
